5b0849b261b85b794640c5cae2de024ba7332f08
Elena Pavlova <lenlho22@gmail.com>
2016-09-15 23:42:35 +0300

    Рефакторинг кода (подключение шрифта OpenSans, добавление svg, структура файлов sass)

---
+++ js/adaptive-menu.js
@@ -0,0 +1,12 @@
+;(function() {
+  var navToggler = document.getElementById('navToggler'),
+    mainNav = document.getElementById('mainNav'),
+    pageHeader = document.querySelector('.page-header');
+
+  navToggler.addEventListener('click', function() {
+    this.classList.toggle('main-nav__toggler--close');
+    mainNav.classList.toggle('main-nav__list--opened');
+    mainNav.parentElement.classList.toggle('main-nav--opened');
+    pageHeader.classList.toggle('page-header--bg-position');
+  });
+})();
